831a42868a677a53ec4efb7ad4c38fd16a64220b
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-25 11:09:30 +0530

    tests ranges within a set -- lowerbound upperbound

---
+++ setrange.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<set>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    set<string, less<string> > organic;
+
+    set<string, less<string> >::iterator iter;
+
+    organic.insert("Curine");
+    organic.insert("Xanthine");
+    organic.insert("Curarine");
+    organic.insert("Melamine");
+    organic.insert("Cyanimide");
+    organic.insert("Phenol");
+    organic.insert("Aphrodine");
+    organic.insert("Imidazole");
+    organic.insert("Cinchonine");
+    organic.insert("Palmitamide");
+    organic.insert("Cyanimide");
+
+    iter = organic.begin();
+    while (iter != organic.end())
+    {
+        cout <<*iter++ <<'\n';
+    }
+
+    string lower, upper; 
+    cout<<" \nEnter range (example C Czz): ";
+
+    cin >> lower >> upper;
+
+    iter = organic.lower_bound(lower);
+
+    while( iter != organic.upper_bound(upper)){
+        cout << *iter++ <<'\n';
+    }
+    return 0;
+    
+}
